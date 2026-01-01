8a6f6a85cf638077731b81fb50bc43e2dc1db717
taazcode <taazpaul06@gmail.com>
2026-01-01 10:50:18 +0530

    Add prime number checking program in prime.c

---
+++ prime.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+void main() {
+    int n, i, flag = 0;
+    clrscr();
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    if (n <= 1) flag = 1;
+    for (i = 2; i <= n/2; i++) {
+        if (n % i == 0) {
+            flag = 1;
+            break;
+        }
+    }
+    if (flag == 0) printf("%d is Prime", n);
+    else printf("%d is Not Prime", n);
+    getch();
+}
